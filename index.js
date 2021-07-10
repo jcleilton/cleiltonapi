af8000e121190a44609e46c9ac2e546d14d8a540
Cleilton <j.cleilton.feitosa@accenture.com>
2021-07-10 17:00:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+let port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.listen(port, () => {
+    console.log("listening on " + port)
+})
